004476c144bb67abeb20e6d5b41c88af41ba4277
justlogicalahmad <rabiu2993@gmail.com>
2024-09-23 23:46:57 -0700

    Install chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,8 @@
 import "./App.css";
-import {
-  ChakraBaseProvider,
-  extendBaseTheme,
-  theme as chakraTheme,
-} from "@chakra-ui/react";
 import Like from "./components/Like";
 
-const { Button } = chakraTheme.components;
-
-const theme = extendBaseTheme({
-  components: {
-    Button,
-  },
-});
-
 function App() {
-  return (
-    <ChakraBaseProvider theme={theme}>
-      <Like likeSize={30} />
-    </ChakraBaseProvider>
-  );
+  return <Like likeSize={30} />;
 }
 
 export default App;
