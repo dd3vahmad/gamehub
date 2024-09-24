0b5a2aafee72c3f788768e70228450dbc9a8a35a
justlogicalahmad <rabiu2993@gmail.com>
2024-09-23 23:56:13 -0700

    Built a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
-import Like from "./components/Like";
+// import Like from "./components/Like";
 
 function App() {
-  return <Like likeSize={30} />;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area={"nav"} bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"aside"} bg="gold">
+          aside
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
