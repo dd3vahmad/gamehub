6cf953c2a3b0e31078e561635699cb8e7652c76c
justlogicalahmad <rabiu2993@gmail.com>
2024-09-25 14:45:51 -0700

    Add emojis

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,20 @@
+import { Image, ImageProps } from "@chakra-ui/react";
+import bullsEye from "../assets/bulls-eye.webp";
+import meh from "../assets/meh.webp";
+import thumbsUp from "../assets/thumbs-up.webp";
+
+interface Props {
+  rating: number;
+}
+
+const Emoji = ({ rating }: Props) => {
+  const emojiMap: { [key: number]: ImageProps } = {
+    3: { src: meh, alt: "meh", boxSize: "25px" },
+    4: { src: thumbsUp, alt: "recommended", boxSize: "25px" },
+    5: { src: bullsEye, alt: "exceptional", boxSize: "35px" },
+  };
+
+  return <Image marginTop={1} {...emojiMap[rating]} />;
+};
+
+export default Emoji;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/react.svg";
+import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
