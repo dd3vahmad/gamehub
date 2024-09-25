29bc0d7faf6ac967c452c1d25cc0aa05b7a96379
justlogicalahmad <rabiu2993@gmail.com>
2024-09-25 13:58:24 -0700

    Build search input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -10,7 +10,7 @@ const ColorModeSwitch = () => {
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       />
-      <Text>DarkMode</Text>
+      <Text whiteSpace={"nowrap"}>Dark Mode</Text>
     </HStack>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/react.svg";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const Navbar = () => {
   return (
-    <HStack justifyContent="space-between" p="10px">
+    <HStack p="10px">
       <Image src={logo} boxSize={"60px"} alt="Gaymz Logo" />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
