e68145af86695195f90f5dde5d00ff4ef9a8689f
justlogicalahmad <rabiu2993@gmail.com>
2024-10-01 01:03:27 -0700

    Refacto Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,41 +1,40 @@
-import { Box, Flex, Image } from "@chakra-ui/react";
+import {
+  Box,
+  HStack,
+  Image,
+  useDisclosure,
+  useMediaQuery,
+} from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
-import { useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Drawer from "./Drawer";
-import { useState } from "react";
 
 const Navbar = () => {
-  const navigate = useNavigate();
-  const [isOpen, setIsOpen] = useState(false);
-
-  const isMobileAndHome = true;
-
-  const onClose = () => {
-    return setIsOpen(false);
-  };
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isMobile] = useMediaQuery("(max-width: 800px)");
+  const { pathname } = useLocation();
 
   return (
-    <Flex p="10px" alignItems="center" gap={5}>
-      <Image
-        onClick={() => {
-          if (isMobileAndHome) {
-            return setIsOpen(true);
-          }
-          navigate("/");
-        }}
-        src={logo}
-        boxSize={"60px"}
-        alt="Gaymz Logo"
-        cursor="pointer"
-      />
+    <HStack padding="10px" gap={{ base: 2, lg: 6 }}>
+      <Link to="/">
+        <Image
+          onClick={() => {
+            if (pathname === "/" && isMobile) onOpen();
+          }}
+          src={logo}
+          boxSize={"60px"}
+          alt="Gaymz Logo"
+          cursor="pointer"
+        />
+      </Link>
       <Box flex="1">
         <SearchInput />
       </Box>
       <ColorModeSwitch />
-      {isOpen && <Drawer isOpen={isOpen} placement="left" onClose={onClose} />}
-    </Flex>
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose} />
+    </HStack>
   );
 };
 
