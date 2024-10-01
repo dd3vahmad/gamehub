44dcf155e4a2246b2a4f54865c19edd042ebfdf4
justlogicalahmad <rabiu2993@gmail.com>
2024-10-01 02:07:07 -0700

    Fix error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,23 +1,19 @@
 import { Box, Heading, Text } from "@chakra-ui/react";
 import { isRouteErrorResponse, useRouteError } from "react-router-dom";
-import Navbar from "../components/Navbar";
 
 const Error = () => {
   const error = useRouteError();
   const isRouteError = isRouteErrorResponse(error);
 
   return (
-    <>
-      <Navbar />
-      <Box padding={5}>
-        <Heading>Oops</Heading>{" "}
-        <Text>
-          {isRouteError
-            ? "This page does not exist"
-            : "An unexpected error occurred"}
-        </Text>
-      </Box>
-    </>
+    <Box padding={5}>
+      <Heading>Oops</Heading>{" "}
+      <Text>
+        {isRouteError
+          ? "This page does not exist"
+          : "An unexpected error occurred"}
+      </Text>
+    </Box>
   );
 };
 
